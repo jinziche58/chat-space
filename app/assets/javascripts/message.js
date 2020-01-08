6110077e0a25380c4be16df7d48e5d703b8d97fe
{アカウント名} <mail@gmail.com>
2020-01-08 16:23:03 +0900

    chat-space自動更新機能再修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -46,24 +46,18 @@ $(function(){
   var reloadMessages = function() {
   last_message_id = $('.message:last').data("message-id");
       $.ajax({
-        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
         url: "api/messages",
-        //ルーティングで設定した通りhttpメソッドをgetに指定
         type: 'get',
         dataType: 'json',
-        //dataオプションでリクエストに値を含める
         data: {id: last_message_id}
       })
       .done(function(messages) {
         console.log('success');
         if (messages.length !== 0) {
-          //追加するHTMLの入れ物を作る
           var insertHTML = '';
-          //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
           $.each(messages, function(i, message) {
             insertHTML += buildHTML(message)
           });
-          //メッセージが入ったHTMLに、入れ物ごと追加
           $('.messages').append(insertHTML);
           $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
           $("#new_message")[0].reset();
@@ -103,7 +97,6 @@ $(function(){
     alert("メッセージ送信に失敗しました");
    });   
    return false;
-   //$(function(){});の閉じタグの直上(処理の最後)に以下のように追記
   });
 
 });
