d539d3c360645b8a0c05708fcefb3ae3946ffd27
{アカウント名} <mail@gmail.com>
2020-01-13 21:03:54 +0900

    change

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :rbenv_ruby, '2.5.3' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['yibaohang716：~/.ssh/key_pem.pem）>'] 
+                  keys: ['~/.ssh/yibaohang716.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
