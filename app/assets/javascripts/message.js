03ac27ccc1136b49b1b6c9b5fbf18f1939856b74
{アカウント名} <mail@gmail.com>
2020-01-21 19:46:52 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,44 +1,75 @@
 $(function(){ 
-    function buildHTML(message){
-     if ( message.image ) {
-       var html =
-        `<div class="message" data-message-id=${message.id}>
-           <div class="upper-message">
-             <div class="upper-message__user-name">
-               ${message.user_name}
-             </div>
-             <div class="upper-message__date">
-               ${message.created_at}
-             </div>
-           </div>
-           <div class="lower-message">
-             <p class="lower-message__content">
-               ${message.content}
-             </p>
-           </div>
-           <img src=${message.image} >
-         </div>`
-       return html;
-     } else {
-       var html =
-        `<div class="message" data-message-id=${message.id}>
-           <div class="upper-message">
-             <div class="upper-message__user-name">
-               ${message.user_name}
-             </div>
-             <div class="upper-message__date">
-               ${message.created_at}
-             </div>
-           </div>
-           <div class="lower-message">
-             <p class="lower-message__content">
-               ${message.content}
-             </p>
-           </div>
-         </div>`
-       return html;
-     };
-   }
+
+  var buildHTML = function(message) {
+
+    if ( message.image ) {
+      var html =
+       `<div class="message" data-message-id=${message.id}>
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    } else {
+      var html =
+       `<div class="message" data-message-id=${message.id}>
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  };
+  
+   
+  var reloadMessages = function() {
+  last_message_id = $('.message:last').data("message-id");
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        if (messages.length !== 0) {
+          var insertHTML = '';
+          $.each(messages, function(i, message) {
+            insertHTML += buildHTML(message)
+          });
+          $('.messages').append(insertHTML);
+          $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+          $("#new_message")[0].reset();
+          $(".form__submit").prop("disabled", false);
+        }
+      })
+
+      .fail(function() {
+        alert("自動更新に失敗しました")
+      });
+  }
+  setInterval(reloadMessages, 7000);
+
  $('#new_message').on('submit', function(e){
    e.preventDefault();
    var formData = new FormData(this);
@@ -65,4 +96,5 @@ $(function(){
    });   
    return false;
   });
+
 });
