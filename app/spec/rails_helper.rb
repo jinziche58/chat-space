df0e755873423ad7fb85b00309cfe942a05ac962
{アカウント名} <mail@gmail.com>
2020-02-21 19:24:22 +0900

    a

--- app/spec/rails_helper.rb
+++
@@ -1,8 +0,0 @@
-# require 'spec_helper'
-# .configure do |config|
-#     Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-#     config.include Devise::Test::ControllerHelpers, type: :controller
-#     config.include ControllerMacros, type: :controller
-#     #〜省略〜
-#     require 'capybara/rspec'
-#   end
--- app/spec/support/controller_macros.rb
+++
@@ -1,7 +0,0 @@
-module ControllerMacros
-    def login(user)
-      @request.env["devise.mapping"] = Devise.mappings[:user]
-      sign_in user
-    end
-  end
-  
